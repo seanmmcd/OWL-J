77db7a623e2a52b36dc44ff53fe7a33da825bf7b
Sean McDonough <smcdonough@umassd.edu>
2018-10-02 00:46:51 -0400

    Icon clicking processing.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -90,8 +90,14 @@ public class Main extends Application {
         initializePacificButtons(OWL_DALLAS_FUEL, OWL_LA_GLADIATORS, OWL_LA_VALIANT, OWL_SEOUL_DYNASTY, OWL_SF_SHOCK, OWL_SHAI_DRAGONS, homeScreen);
 
         //ATLANTIC TEAMS
+        ImageView OWL_BOS_UPRISING  = new ImageView(new Image("/sample/OWL/ATLANTIC/OWL_BOS_UPRISING.png"));
+        ImageView OWL_FLA_MAYHEM    = new ImageView(new Image("/sample/OWL/ATLANTIC/OWL_FLOR_MAYHEM.png"));
+        ImageView OWL_HOU_OUTLAWS   = new ImageView(new Image("/sample/OWL/ATLANTIC/OWL_HOUS_OUTLAWS.png"));
+        ImageView OWL_LON_SPITFIRE  = new ImageView(new Image("/sample/OWL/ATLANTIC/OWL_LNDN_SPITFIRE.png"));
+        ImageView OWL_NYC_EXCEL     = new ImageView(new Image("/sample/OWL/ATLANTIC/OWL_NY_XL.png"));
+        ImageView OWL_PHI_FUSION    = new ImageView(new Image("/sample/OWL/ATLANTIC/OWL_PHILLY_FUSION.png"));
 
-
+        initializeAtlanticButtons(OWL_BOS_UPRISING, OWL_FLA_MAYHEM, OWL_HOU_OUTLAWS, OWL_LON_SPITFIRE, OWL_NYC_EXCEL, OWL_PHI_FUSION, homeScreen);
 
         atlanticImage.setOnMouseClicked(new EventHandler<MouseEvent>() {
             public void handle(MouseEvent me) {
@@ -104,19 +110,75 @@ public class Main extends Application {
             }
         });
         owwcIcon.setOnMouseClicked(new EventHandler<MouseEvent>() {
+               public void handle(MouseEvent me) {
+                   window.setScene(new Scene(teamPage));
+               }});
+
+
+        //PACIFIC TEAM ICON CLICK PROCESSING
+        OWL_DALLAS_FUEL.setOnMouseClicked(new EventHandler<MouseEvent>() {
             public void handle(MouseEvent me) {
-                window.setScene(new Scene(teamPage));
+                changeToTeamPage(0, window, new Scene(teamPage));
+            }
+        });
+        OWL_LA_GLADIATORS.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent me) {
+                changeToTeamPage(1, window, new Scene(teamPage));
+            }
+        });
+        OWL_LA_VALIANT.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent me) {
+                changeToTeamPage(2, window, new Scene(teamPage));
+            }
+        });
+        OWL_SEOUL_DYNASTY.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent me) {
+                changeToTeamPage(3, window, new Scene(teamPage));
+            }
+        });
+        OWL_SF_SHOCK .setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent me) {
+                changeToTeamPage(4, window, new Scene(teamPage));
+            }
+        });
+        OWL_SHAI_DRAGONS.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent me) {
+                changeToTeamPage(5, window, new Scene(teamPage));
             }
         });
 
 
-
-
-
-
-
-
-
+        //ATLANTIC TEAM ICON CLICK PROCESSING
+        OWL_BOS_UPRISING.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent me) {
+                changeToTeamPage(6, window, new Scene(teamPage));
+            }
+        });
+        OWL_FLA_MAYHEM.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent me) {
+                changeToTeamPage(7, window, new Scene(teamPage));
+            }
+        });
+        OWL_HOU_OUTLAWS.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent me) {
+                changeToTeamPage(8, window, new Scene(teamPage));
+            }
+        });
+        OWL_LON_SPITFIRE.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent me) {
+                changeToTeamPage(9, window, new Scene(teamPage));
+            }
+        });
+        OWL_NYC_EXCEL.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent me) {
+                changeToTeamPage(10, window, new Scene(teamPage));
+            }
+        });
+        OWL_PHI_FUSION.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent me) {
+                changeToTeamPage(11, window, new Scene(teamPage));
+            }
+        });
 
 
         Font.loadFont(getClass().getResourceAsStream("/sample/OWL/bignoodletoo.ttf"), 14);
@@ -166,45 +228,45 @@ public class Main extends Application {
     }
 
     public void initializePacificButtons(ImageView fuel, ImageView glad, ImageView vali, ImageView dyna, ImageView shock, ImageView drag, Pane homeScreen){
-        fuel.setFitHeight(158.0);
-        fuel.setFitWidth(324.0);
-        fuel.setLayoutX(622.0);
-        fuel.setLayoutY(328.0);
+        fuel.setFitHeight(86);
+        fuel.setFitWidth(41);
+        fuel.setLayoutX(14);
+        fuel.setLayoutY(279);
         fuel.setPickOnBounds(true);
         fuel.setPreserveRatio(true);
 
-        glad.setFitHeight(158.0);
-        glad.setFitWidth(324.0);
-        glad.setLayoutX(622.0);
-        glad.setLayoutY(328.0);
+        glad.setFitHeight(86);
+        glad.setFitWidth(41);
+        glad.setLayoutX(55);
+        glad.setLayoutY(279);
         glad.setPickOnBounds(true);
         glad.setPreserveRatio(true);
 
-        vali.setFitHeight(158.0);
-        vali.setFitWidth(324.0);
-        vali.setLayoutX(622.0);
-        vali.setLayoutY(328.0);
+        vali.setFitHeight(86);
+        vali.setFitWidth(41);
+        vali.setLayoutX(106);
+        vali.setLayoutY(279);
         vali.setPickOnBounds(true);
         vali.setPreserveRatio(true);
 
-        dyna.setFitHeight(158.0);
-        dyna.setFitWidth(324.0);
-        dyna.setLayoutX(622.0);
-        dyna.setLayoutY(328.0);
+        dyna.setFitHeight(86);
+        dyna.setFitWidth(41);
+        dyna.setLayoutX(254);
+        dyna.setLayoutY(279);
         dyna.setPickOnBounds(true);
         dyna.setPreserveRatio(true);
 
-        shock.setFitHeight(158.0);
-        shock.setFitWidth(324.0);
-        shock.setLayoutX(622.0);
-        shock.setLayoutY(328.0);
+        shock.setFitHeight(86);
+        shock.setFitWidth(41);
+        shock.setLayoutX(154);
+        shock.setLayoutY(279);
         shock.setPickOnBounds(true);
         shock.setPreserveRatio(true);
 
-        drag.setFitHeight(158.0);
-        drag.setFitWidth(324.0);
-        drag.setLayoutX(622.0);
-        drag.setLayoutY(328.0);
+        drag.setFitHeight(86);
+        drag.setFitWidth(41);
+        drag.setLayoutX(205);
+        drag.setLayoutY(279);
         drag.setPickOnBounds(true);
         drag.setPreserveRatio(true);
 
@@ -214,45 +276,45 @@ public class Main extends Application {
     }
 
     public void initializeAtlanticButtons(ImageView rise, ImageView mayh, ImageView outl, ImageView spit, ImageView excel, ImageView fusion, Pane homeScreen){
-        rise.setFitHeight(158.0);
-        rise.setFitWidth(324.0);
-        rise.setLayoutX(622.0);
-        rise.setLayoutY(328.0);
+        rise.setFitHeight(86);
+        rise.setFitWidth(41);
+        rise.setLayoutX(621);
+        rise.setLayoutY(279);
         rise.setPickOnBounds(true);
         rise.setPreserveRatio(true);
 
-        mayh.setFitHeight(158.0);
-        mayh.setFitWidth(324.0);
-        mayh.setLayoutX(622.0);
-        mayh.setLayoutY(328.0);
+        mayh.setFitHeight(86);
+        mayh.setFitWidth(41);
+        mayh.setLayoutX(662);
+        mayh.setLayoutY(272);
         mayh.setPickOnBounds(true);
         mayh.setPreserveRatio(true);
 
-        outl.setFitHeight(158.0);
-        outl.setFitWidth(324.0);
-        outl.setLayoutX(622.0);
-        outl.setLayoutY(328.0);
+        outl.setFitHeight(86);
+        outl.setFitWidth(41);
+        outl.setLayoutX(710);
+        outl.setLayoutY(272);
         outl.setPickOnBounds(true);
         outl.setPreserveRatio(true);
 
-        spit.setFitHeight(158.0);
-        spit.setFitWidth(324.0);
-        spit.setLayoutX(622.0);
-        spit.setLayoutY(328.0);
+        spit.setFitHeight(86);
+        spit.setFitWidth(41);
+        spit.setLayoutX(762);
+        spit.setLayoutY(272);
         spit.setPickOnBounds(true);
         spit.setPreserveRatio(true);
 
-        excel.setFitHeight(158.0);
-        excel.setFitWidth(324.0);
-        excel.setLayoutX(622.0);
-        excel.setLayoutY(328.0);
+        excel.setFitHeight(86);
+        excel.setFitWidth(41);
+        excel.setLayoutX(811);
+        excel.setLayoutY(272);
         excel.setPickOnBounds(true);
         excel.setPreserveRatio(true);
 
-        fusion.setFitHeight(158.0);
-        fusion.setFitWidth(324.0);
-        fusion.setLayoutX(622.0);
-        fusion.setLayoutY(328.0);
+        fusion.setFitHeight(86);
+        fusion.setFitWidth(41);
+        fusion.setLayoutX(860);
+        fusion.setLayoutY(272);
         fusion.setPickOnBounds(true);
         fusion.setPreserveRatio(true);
 
@@ -261,7 +323,25 @@ public class Main extends Application {
         return;
     }
 
+    public void changeToTeamPage(int teamID, Stage window, Scene teamPage){
+        //prepTeamScene(teamID, teamPage);
+        window.setScene(teamPage);
+        return;
+    }
+
+    public void changeToDivisionPage(int divID){
+        //prepDivScene
+
+        return;
+    }
+
     public static void main(String[] args) {
         launch(args);
+    }
+}
+
+class scenePrep{
+    public static void ok(){
+
     }
 }
